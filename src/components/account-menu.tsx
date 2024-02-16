798e74a3612e4b8b37e303e32ba53c74ea941927
Edilaine Alves dos Santos <91744159+edilaine-as@users.noreply.github.com>
2024-02-16 15:02:34 -0300

    Add Dark Mode

---
+++ src/components/account-menu.tsx
@@ -0,0 +1,42 @@
+import { Building, ChevronDown } from 'lucide-react'
+
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex select-none items-center gap-2"
+        >
+          Pizza Shop
+          <ChevronDown className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Edilaine Santos</span>
+          <span className="text-sm font-normal text-muted-foreground">
+            edilaine.santos@gmail.com
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuItem>
+          <Building className="mr-2 h-4 w-4" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+          <Building className="mr-2 h-4 w-4" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
