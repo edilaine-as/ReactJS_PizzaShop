7c2387222d03ddd9b54df1dedc531d45a6c242bc
Edilaine Santos <edilaine.santos839@gmail.com>
2024-02-27 21:33:56 -0300

    Add profile and managedRestaurant requests

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,4 +4,5 @@ import { env } from '@/env'
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
+  withCredentials: true, // permite que os cookies do front-end sejam automaticamente enviados pro back-end
 })
