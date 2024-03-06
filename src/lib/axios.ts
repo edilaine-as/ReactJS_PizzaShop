5c65fe8a40d13c36820bac395026a1151440cfe1
Edilaine Santos <edilaine.santos839@gmail.com>
2024-03-05 21:27:27 -0300

    Add skeleton

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,3 +6,12 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true, // permite que os cookies do front-end sejam automaticamente enviados pro back-end
 })
+
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    // intercepta cada requisição e retorna os dados da minha requisição (corpo, cabeçalho), já que o interceptions permite que a gente customize os dados da requisição
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    return config
+  })
+}
