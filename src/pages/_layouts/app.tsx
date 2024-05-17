9ee9ec90fe5d36a30ac8e07086940f922830adde
Edilaine Santos <edilaine.santos839@gmail.com>
2024-05-16 22:17:37 -0300

    Working on authentication validation

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,8 +1,36 @@
-import { Outlet } from 'react-router-dom'
+import { isAxiosError } from 'axios'
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 import { Header } from '@/components/header'
+import { api } from '@/lib/axios'
 
 export function AppLayout() {
+  const navigate = useNavigate()
+
+  // evita que o usuário navegue no dashboard sem estar autenticado, redirecionando ele até a página de autentificação
+  useEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      // o use recebe duas funções: a primeira é disparada quando há sucesso, a segunda quando dá erro
+      (response) => response,
+      (error) => {
+        if (isAxiosError(error)) {
+          const status = error.response?.status
+          const code = error.response?.data.code
+
+          if (status === 401 && code === 'UNAUTHORIZED') {
+            navigate('/sign-in', { replace: true })
+            // replace true faz com que o usuario não consiga voltar usando o back-link do navegador
+          }
+        }
+      },
+    )
+
+    return () => {
+      api.interceptors.response.eject(interceptorId)
+    }
+  }, [navigate])
+
   return (
     <div className="flex min-h-screen flex-col antialiased">
       <Header />
